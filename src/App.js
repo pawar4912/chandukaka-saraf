95f48cb051b9327d27cbc81ac17c88f349877de0
pawar4912 <pawar2000swapnil@gmail.com>
2023-10-12 00:05:54 +0530

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import routes from './routes';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Header />
+        <div>
+          <Suspense>
+            <Routes>
+              {routes.map((route, idx) => {
+                return (
+                  route.element && (
+                    <Route
+                      key={idx}
+                      path={route.path}
+                      element={route.element}
+                      exact={route.exact}
+                      strict={route.strict}
+                    />
+                  )
+                );
+              })}
+            </Routes>
+          </Suspense>
+        </div>
+        <Footer />
+      </BrowserRouter>
   );
 }
 
-export default App;
+export default App
---
+++ src/routes.js
@@ -0,0 +1,20 @@
+import Home from './Components/Screens/Home'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    element: <Home/>,
+    exact: true,
+    strict: false,
+  },
+  {
+    path: '/test',
+    name: 'Home',
+    element: <Home/>,
+    exact: true,
+    strict: false,
+  }
+]
+
+export default routes;
