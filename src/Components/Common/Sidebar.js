48e61f9fc164d75b8e524cfeb26d3ac8e802c143
pawar4912 <86315952+pawar4912@users.noreply.github.com>
2023-11-13 12:28:41 +0530

    [wip]

--- src/Components/Common/Sidebar.js
+++ src/Components/Common/Sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { ListItemIcon } from "@mui/material";
 import orderIcon from '../../images/icons/sidebar/orders-icon.svg';
 import personalDetailsIcon from '../../images/icons/sidebar/personal-details-icon.svg';
@@ -7,82 +7,168 @@ import heartIcon from '../../images/icons/sidebar/heart.svg';
 import helpIcon from '../../images/icons/sidebar/help-icon.svg';
 import logoutIcon from '../../images/icons/sidebar/logout-icon.svg';
 import web from "../../images/Group265.png";
+import { SwipeableDrawer } from "@mui/material";
 
 export default function Sidebar() {
+  const [isOpen, setIsOpen] = useState(false)
+  const openDrawer = () => {
+    setIsOpen(true)
+  }
+  const closeDrawer = () => {
+    setIsOpen(false)
+  }
+
   return (
     <>
       <div className="sidebar">
-        <ul className="menu-wrapper">
-          <li className="w-100 d-flex profile-wrapper">
-            <img className="profile-img" src={web} />
-            <div className="profile-info">
-              <b className="name">Hi Smita</b>
-              <small>+91 9480000032</small>
+        <div className=" d-none d-md-block">
+          <ul className="menu-wrapper">
+            <li className="w-100 d-flex profile-wrapper">
+              <img className="profile-img" src={web} />
+              <div className="profile-info">
+                <b className="name">Hi Smita</b>
+                <small>+91 9480000032</small>
+              </div>
+            </li>
+            <div className="sidebar-menu">
+              <li className="w-100">
+                <a href="#" className="menu-link">
+                  <img
+                    src={orderIcon}
+                    alt="Logo"
+                    className="image"
+                  />
+                  MY ORDERS
+                </a>
+              </li>
+              <li className="w-100">
+                <a href="#" className="menu-link">
+                  <img
+                    src={personalDetailsIcon}
+                    alt="Logo"
+                    className="image"
+                  />
+                  PERSONAL DETAILS
+                </a>
+              </li>
+              <li className="w-100">
+                <a href="#" className="menu-link">
+                  <img
+                    src={addressBookIcon}
+                    alt="Logo"
+                    className="image"
+                  />
+                  ADDRESS BOOK
+                </a>
+              </li>
+              <li className="w-100">
+                <a href="#" className="menu-link">
+                  <img
+                    src={heartIcon}
+                    alt="Logo"
+                    className="image"
+                  />
+                  WISHLIST
+                </a>
+              </li>
+              <li className="w-100">
+                <a href="#" className="menu-link">
+                  <img
+                    src={helpIcon}
+                    alt="Logo"
+                    className="image"
+                  />
+                  SUPPORT
+                </a>
+              </li>
             </div>
-          </li>
-          <div className="sidebar-menu">
-            <li className="w-100">
+            <li className="w-100 sign-out">
               <a href="#" className="menu-link">
                 <img
-                  src={orderIcon}
+                  src={logoutIcon}
                   alt="Logo"
                   className="image"
                 />
-                MY ORDERS
+                SIGN OUT
               </a>
             </li>
-            <li className="w-100">
+          </ul>
+        </div>
+        <SwipeableDrawer className="mobile-drawer" open={isOpen} onOpen={openDrawer} onClose={closeDrawer} swipeAreaWidth={100}>
+          <ul className="menu-wrapper">
+            <li className="w-100 d-flex profile-wrapper">
+              <div className="profile-section w-100 d-flex">
+                <img className="profile-img" src={web} />
+                <div className="profile-info">
+                  <b className="name">Hi Smita</b>
+                  <small>+91 9480000032</small>
+                </div>
+              </div>
+            </li>
+            <div className="sidebar-menu">
+              <li className="w-100">
+                <a href="#" className="menu-link">
+                  <img
+                    src={orderIcon}
+                    alt="Logo"
+                    className="image"
+                  />
+                  MY ORDERS
+                </a>
+              </li>
+              <li className="w-100">
+                <a href="#" className="menu-link">
+                  <img
+                    src={personalDetailsIcon}
+                    alt="Logo"
+                    className="image"
+                  />
+                  PERSONAL DETAILS
+                </a>
+              </li>
+              <li className="w-100">
+                <a href="#" className="menu-link">
+                  <img
+                    src={addressBookIcon}
+                    alt="Logo"
+                    className="image"
+                  />
+                  ADDRESS BOOK
+                </a>
+              </li>
+              <li className="w-100">
+                <a href="#" className="menu-link">
+                  <img
+                    src={heartIcon}
+                    alt="Logo"
+                    className="image"
+                  />
+                  WISHLIST
+                </a>
+              </li>
+              <li className="w-100">
+                <a href="#" className="menu-link">
+                  <img
+                    src={helpIcon}
+                    alt="Logo"
+                    className="image"
+                  />
+                  SUPPORT
+                </a>
+              </li>
+            </div>
+            <li className="w-100 sign-out">
               <a href="#" className="menu-link">
                 <img
-                  src={personalDetailsIcon}
+                  src={logoutIcon}
                   alt="Logo"
                   className="image"
                 />
-                PERSONAL DETAILS
+                SIGN OUT
               </a>
             </li>
-            <li className="w-100">
-              <a href="#" className="menu-link">
-                <img
-                  src={addressBookIcon}
-                  alt="Logo"
-                  className="image"
-                />
-                ADDRESS BOOK
-              </a>
-            </li>
-            <li className="w-100">
-              <a href="#" className="menu-link">
-                <img
-                  src={heartIcon}
-                  alt="Logo"
-                  className="image"
-                />
-                WISHLIST
-              </a>
-            </li>
-            <li className="w-100">
-              <a href="#" className="menu-link">
-                <img
-                  src={helpIcon}
-                  alt="Logo"
-                  className="image"
-                />
-                SUPPORT
-              </a>
-            </li>
-          </div>
-          <li className="w-100 sign-out">
-            <a href="#" className="menu-link">
-              <img
-                src={logoutIcon}
-                alt="Logo"
-                className="image"
-              />
-              SIGN OUT
-            </a>
-          </li>
-        </ul>
+          </ul>
+        </SwipeableDrawer>
       </div>
     </>
   )
