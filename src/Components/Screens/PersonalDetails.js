e54c8e17a067370951586cc4d2d6f98e80476e6c
aniket salunke <aniket3984@gmail.com>
2023-12-18 23:50:14 +0530

    refactored code for personal details

--- src/Components/Screens/PersonalDetails.js
+++ src/Components/Screens/PersonalDetails.js
@@ -8,14 +8,63 @@ import {
   Grid,
   TextField,
   Button,
-  ThemeProvider,
-  createTheme,
 } from "@mui/material";
+import EastIcon from "@mui/icons-material/East";
 
 export const PersonalDetails = () => {
+  
+  const days = Array.from({ length: 31 }, (_, i) => i + 1);
+  
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  
+  const currentYear = new Date().getFullYear();
+  const years = Array.from({ length: 100 }, (_, i) => currentYear - i);
+
+  const [title, setTitle] = useState("mrs");
+  const [gender, setGender] = useState("female");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
   const [date, setDate] = useState(1);
   const [month, setMonth] = useState("");
   const [year, setYear] = useState("");
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    switch (name) {
+      case "title":
+        setTitle(value);
+        break;
+      case "gender":
+        setGender(value);
+        break;
+      case "firstName":
+        setFirstName(value);
+        break;
+      case "lastName":
+        setLastName(value);
+        break;
+      case "email":
+        setEmail(value);
+        break;
+      default:
+        break;
+    }
+  };
 
   const handleDateChange = (event) => {
     setDate(event.target.value);
@@ -29,193 +78,188 @@ export const PersonalDetails = () => {
     setYear(event.target.value);
   };
 
-  const customTheme = createTheme({
-    components: {
-      MuiOutlinedInput: {
-        styleOverrides: {
-          root: {
-            border: "none",
-          },
-        },
-      },
-      MuiFormControl: {
-        styleOverrides: {
-          root: {
-            border: "none",
-          },
-        },
-      },
-      MuiSelect: {
-        styleOverrides: {
-          select: {
-            "&:focus": {
-              backgroundColor: "transparent",
-            },
-          },
-        },
-      },
-      MuiButton: {
-        styleOverrides: {
-          root: {
-            border: "none",
-          },
-        },
-      },
-    },
-  });
+  () => {
+    setDate(days[0]);
+    setMonth(months[0]);
+    setYear(currentYear - 20);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("form values", {
+      title: title,
+      gender: gender,
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      date: date,
+      month: month,
+      year: year,
+    });
+  };
 
   return (
-    <ThemeProvider theme={customTheme}>
-      <div className="d-flex">
-        <Sidebar></Sidebar>
+    <div className="d-flex">
+      <Sidebar></Sidebar>
 
-        <div className="personal-details-container p-3">
-          <h5>PERSONAL DETAILS</h5>
+      <div className="personal-details-container p-3">
+        <h5>PERSONAL DETAILS</h5>
 
-          <div className="personal-details-form p-3">
-            <form>
-              <Grid container spacing={2}>
-                <Grid item xs={12} md={6}>
-                  <FormControl fullWidth>
-                    <InputLabel id="select-title">Title</InputLabel>
-                    <Select
-                      labelId="select-title"
-                      id="title-dropdown"
-                      label="Title"
-                      value={"mrs"}
-                      // onChange={handleChange}
-                    >
-                      <MenuItem value="mrs">Mrs</MenuItem>
-                      <MenuItem value="mr">Mr</MenuItem>
-                    </Select>
-                  </FormControl>
-                </Grid>
-                <Grid item xs={12} md={6}>
-                  <FormControl
-                    fullWidth
+        <div className="personal-details-form p-3">
+          <form onSubmit={handleSubmit}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={6}>
+                <FormControl fullWidth>
+                  <InputLabel id="select-title">Title</InputLabel>
+                  <Select
+                    labelId="select-title"
+                    id="title-dropdown"
+                    label="Title"
+                    value={"mrs"}
+                    onChange={handleChange}
+                  >
+                    <MenuItem value="mrs">Mrs</MenuItem>
+                    <MenuItem value="mr">Mr</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+
+              <Grid item xs={12} md={6}>
+                <FormControl
+                  fullWidth
+                  sx={{
+                    border: "none",
+                    outline: "none",
+                  }}
+                >
+                  <InputLabel id="select-gender">Gender</InputLabel>
+                  <Select
+                    labelId="select-gender"
+                    id="gender-dropdown"
+                    label="Gender"
+                    value={"female"}
                     sx={{
                       border: "none",
                       outline: "none",
                     }}
+                    onChange={handleChange}
                   >
-                    <InputLabel id="select-gender">Gender</InputLabel>
-                    <Select
-                      labelId="select-gender"
-                      id="gender-dropdown"
-                      label="Gender"
-                      value={"female"}
-                      sx={{
-                        border: "none",
-                        outline: "none",
-                      }}
-                      // onChange={handleChange}
-                    >
-                      <MenuItem value="female">Female</MenuItem>
-                      <MenuItem value="male">Male</MenuItem>
-                    </Select>
-                  </FormControl>
-                </Grid>
-                <Grid item xs={12} md={6}>
-                  <TextField
-                    id="outlined-basic"
-                    label="First Name"
-                    variant="outlined"
-                    required
-                    fullWidth
-                  />
-                </Grid>
-                <Grid item xs={12} md={6}>
-                  <TextField
-                    id="outlined-basic"
-                    label="Last Name"
-                    variant="outlined"
-                    required
-                    fullWidth
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    id="outlined-basic"
-                    label="Email"
-                    variant="outlined"
-                    required
-                    fullWidth
-                  />
-                </Grid>
-
-                <Grid item xs={4}>
-                  <FormControl fullWidth>
-                    <InputLabel id="date-label">Date</InputLabel>
-                    <Select
-                      labelId="date-label"
-                      id="date-dropdown"
-                      value={date}
-                      onChange={handleDateChange}
-                      variant="outlined"
-                      required
-                    >
-                      {[...Array(31).keys()].map((day) => (
-                        <MenuItem key={day + 1} value={day + 1}>
-                          {day + 1}
-                        </MenuItem>
-                      ))}
-                    </Select>
-                  </FormControl>
-                </Grid>
-
-                <Grid item xs={4}>
-                  <FormControl fullWidth>
-                    <InputLabel>Month</InputLabel>
-                    <Select value={month} required onChange={handleMonthChange}>
-                      {[
-                        "January",
-                        "February",
-                        "March",
-                        "April",
-                        "May",
-                        "June",
-                        "July",
-                        "August",
-                        "September",
-                        "October",
-                        "November",
-                        "December",
-                      ].map((m, index) => (
-                        <MenuItem key={index + 1} value={index + 1}>
-                          {m}
-                        </MenuItem>
-                      ))}
-                    </Select>
-                  </FormControl>
-                </Grid>
-
-                <Grid item xs={4}>
-                  <FormControl fullWidth>
-                    <InputLabel>Year</InputLabel>
-                    <Select value={year} onChange={handleYearChange}>
-                      {[...Array(100).keys()].map((yr) => (
-                        <MenuItem key={yr + 1920} value={yr + 1920}>
-                          {yr + 1920}
-                        </MenuItem>
-                      ))}
-                    </Select>
-                  </FormControl>
-                </Grid>
-
-                <Grid item xs={12}>
-                  <Button
-                    className="btn btn-block bg-black"
-                    fullWidth
-                    variant="contained"
-                  >
-                    SAVE CHANGES
-                  </Button>
-                </Grid>
+                    <MenuItem value="female">Female</MenuItem>
+                    <MenuItem value="male">Male</MenuItem>
+                  </Select>
+                </FormControl>
               </Grid>
-            </form>
-          </div>
+
+              <Grid item xs={12} md={6}>
+                <TextField
+                  id="outlined-basic"
+                  label="First Name"
+                  variant="outlined"
+                  sx={{ color: "#000" }}
+                  required
+                  fullWidth
+                  onChange={handleChange}
+                />
+              </Grid>
+
+              <Grid item xs={12} md={6}>
+                <TextField
+                  id="outlined-basic"
+                  label="Last Name"
+                  variant="outlined"
+                  required
+                  fullWidth
+                  onChange={handleChange}
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                  id="outlined-basic"
+                  label="Email"
+                  type="email"
+                  variant="outlined"
+                  required
+                  fullWidth
+                  onChange={handleChange}
+                />
+              </Grid>
+
+              <Grid item xs={4}>
+                <FormControl fullWidth>
+                  <InputLabel id="select-date">Day</InputLabel>
+                  <Select
+                    value={date}
+                    labelId="select-date"
+                    id="day-dropdown"
+                    label="Day"
+                    required
+                    onChange={handleDateChange}
+                  >
+                    {days.map((d) => (
+                      <MenuItem key={d} value={d}>
+                        {d}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </Grid>
+
+              <Grid item xs={4}>
+                <FormControl fullWidth>
+                  <InputLabel id="select-month">Month</InputLabel>
+                  <Select
+                    labelId="select-month"
+                    id="months-dropdown"
+                    label="Month"
+                    required
+                    value={month}
+                    onChange={handleMonthChange}
+                  >
+                    {months.map((m, index) => (
+                      <MenuItem key={index} value={index + 1}>
+                        {m}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </Grid>
+
+              <Grid item xs={4}>
+                <FormControl fullWidth>
+                  <InputLabel id="select-year">Year</InputLabel>
+                  <Select
+                    labelId="select-year"
+                    id="year-dropdown"
+                    label="Year"
+                    required
+                    value={year}
+                    onChange={handleYearChange}
+                  >
+                    {years.map((y) => (
+                      <MenuItem key={y} value={y}>
+                        {y}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </Grid>
+
+              <Grid item xs={12}>
+                <Button
+                  className="btn btn-block bg-black btn-submit"
+                  fullWidth
+                  variant="contained"
+                  type="submit"
+                >
+                  SAVE CHANGES &nbsp;
+                  <EastIcon />
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
         </div>
       </div>
-    </ThemeProvider>
+    </div>
   );
 };
