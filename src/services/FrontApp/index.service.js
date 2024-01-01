7491805d35fe3c3833378aa039d57e429e3b1e96
pawar4912 <86315952+pawar4912@users.noreply.github.com>
2024-01-01 14:59:44 +0530

    login/verify otp API integration

--- src/services/FrontApp/index.service.js
+++ src/services/FrontApp/index.service.js
@@ -4,3 +4,13 @@ const httpService = new UserHTTPService()
 export const getLiveRateForCSPL = async () => {
   return await httpService.get(`getliveRateForCSPL/Z2V0bGl2ZVJhdGVGb3JDU1BM`)
 }
+
+export const login = async (data) => {
+  return await httpService.post(`login`, data)
+}
+
+export const verifyOtp = async (data) => {
+  const result = await httpService.post(`verifyOTP`, data)
+  httpService.saveToken('')
+  console.log(result)
+}
