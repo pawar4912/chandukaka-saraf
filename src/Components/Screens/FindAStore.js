69b1abb8b85dc90c7bb39404b84b4866046030ad
aniket salunke <aniket3984@gmail.com>
2024-01-14 02:20:50 +0530

    InitialChanges

--- src/Components/Screens/FindAStore.js
+++ src/Components/Screens/FindAStore.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import storeLocaterTitleImage from "../../images/storeLocaterTitleImage.png";
+import storeLocaterTitleImage from "../../images/storeLocaterTitleImageCropped.png";
 import { Button, Grid, TextField } from '@mui/material';
 import fillWhiteLeftIcon from '../../images/icons/fillWhiteLeftIcon.svg';
 import fillWhiteRightIcon from '../../images/icons/fillWhiteRightIcon.svg';
@@ -11,102 +11,103 @@ const dataList = [
     {
         title: "BARAMATI",
         description: "Kacheri Road, Mahaveer Path, Gandhi Chowk, Baramati, Maharashtra 413102.",
-        tollFreeNo:"18002670999",
+        tollFreeNo: "18002670999",
         workingHours: "Tue - Sun: 9am - 8pm",
         holiday: "Monday closed"
     },
     {
         title: "RAVIVAR PETH",
         description: "Kacheri Road, Mahaveer Path, Gandhi Chowk, Baramati, Maharashtra 413102.",
-        tollFreeNo:"18002670999",
+        tollFreeNo: "18002670999",
         workingHours: "Tue - Sun: 9am - 8pm",
         holiday: "Monday closed"
     },
     {
         title: "CHINCHWAD",
         description: "Kacheri Road, Mahaveer Path, Gandhi Chowk, Baramati, Maharashtra 413102.",
-        tollFreeNo:"18002670999",
+        tollFreeNo: "18002670999",
         workingHours: "Tue - Sun: 9am - 8pm",
         holiday: "Monday closed"
     },
     {
         title: "CHANDAN NAGAR",
         description: "Kacheri Road, Mahaveer Path, Gandhi Chowk, Baramati, Maharashtra 413102.",
-        tollFreeNo:"18002670999",
+        tollFreeNo: "18002670999",
         workingHours: "Tue - Sun: 9am - 8pm",
         holiday: "Monday closed"
     },
     {
         title: "AKLUJ",
         description: "Kacheri Road, Mahaveer Path, Gandhi Chowk, Baramati, Maharashtra 413102.",
-        tollFreeNo:"18002670999",
+        tollFreeNo: "18002670999",
         workingHours: "Tue - Sun: 9am - 8pm",
         holiday: "Monday closed"
     },
     {
         title: "SATARA",
         description: "Kacheri Road, Mahaveer Path, Gandhi Chowk, Baramati, Maharashtra 413102.",
-        tollFreeNo:"18002670999",
+        tollFreeNo: "18002670999",
         workingHours: "Tue - Sun: 9am - 8pm",
         holiday: "Monday closed"
     },
     {
         title: "KARAD",
         description: "Kacheri Road, Mahaveer Path, Gandhi Chowk, Baramati, Maharashtra 413102.",
-        tollFreeNo:"18002670999",
+        tollFreeNo: "18002670999",
         workingHours: "Tue - Sun: 9am - 8pm",
         holiday: "Monday closed"
     },
     {
         title: "HADAPSAR",
         description: "Kacheri Road, Mahaveer Path, Gandhi Chowk, Baramati, Maharashtra 413102.",
-        tollFreeNo:"18002670999",
+        tollFreeNo: "18002670999",
         workingHours: "Tue - Sun: 9am - 8pm",
         holiday: "Monday closed"
     },
     {
         title: "PUNE-SATARA ROAD",
         description: "Kacheri Road, Mahaveer Path, Gandhi Chowk, Baramati, Maharashtra 413102.",
-        tollFreeNo:"18002670999",
+        tollFreeNo: "18002670999",
         workingHours: "Tue - Sun: 9am - 8pm",
         holiday: "Monday closed"
     },
     {
         title: "BARAMATI MIDC",
         description: "Kacheri Road, Mahaveer Path, Gandhi Chowk, Baramati, Maharashtra 413102.",
-        tollFreeNo:"18002670999",
+        tollFreeNo: "18002670999",
         workingHours: "Tue - Sun: 9am - 8pm",
         holiday: "Monday closed"
     },
 
-  ]
+]
 
 export default function FindAStore() {
     return (
-        <div className='col-12 about-us find-store-main-container'>
-            <Grid container spacing={1}>
-                <Grid item xs={12} md={12} className='product-dash-board-header-image-container find-store-title-image' style={{ backgroundImage: `url(${storeLocaterTitleImage})` }}>
-                    <div className='product-dash-board-image-header-text-container find-store-header-text-container'>
-                        <div className="product-dash-board-header text-only-grid-header">
-                            <img src={fillWhiteLeftIcon} alt="React Logo" />
-                            {'  '}
-                            Store Locator
-                            {' '}
-                            <img src={fillWhiteRightIcon} alt="React Logo" />
-                        </div>
-                        <div className="product-dash-board-description">
-                            Come and say hello. Find your nearest store and check its opening hours and contact details.
-                        </div>
-                        <div className="find-store-change-location">
-                            <TextField placeholder='Enter Pincode or city' className='location-change-text-field' />
-                            <Button className='location-change-button'>FIND STORES</Button>
-                        </div>
-                    </div>
-                </Grid>
-                <Grid item xs={1} md={1}>
-                    <Item className="empty-container-section">xs=6 md=4</Item>
-                </Grid>
-                <Grid className='all-store-container' item xs={12} md={5}>
+        <div className='find-store-main-container'>
+            <div className='section-title' style={{ backgroundImage: `url(${storeLocaterTitleImage})` }}>
+                <div className="header-title">
+                    <img src={fillWhiteLeftIcon} alt="React Logo" />
+                    <span>Store Locator</span>
+                    <img src={fillWhiteRightIcon} alt="React Logo" />
+                </div>
+
+                <div className="section-info">
+                    Come and say hello. Find your nearest store and check its opening hours and contact details.
+                </div>
+
+                <div className="find-store-change-location p-4">
+                    <TextField placeholder='Enter Pincode or city' className='' />
+                    <Button className='location-change-button'>FIND STORES</Button>
+                </div>
+            </div>
+
+
+            <Grid item xs={1} md={1}>
+                <Item className="empty-container-section">xs=6 md=4</Item>
+            </Grid>
+
+
+            {/* <Grid className='all-store-container' item xs={12} md={5}>
                     <div className='all-store-titles'>
                         ALL STORES
                     </div>
@@ -120,8 +121,8 @@ export default function FindAStore() {
                 </Grid>
                 <Grid item xs={1} md={1}>
                     <Item className="empty-container-section">xs=6 md=4</Item>
-                </Grid>
-            </Grid>
+                </Grid> */}
+            {/* </div> */}
         </div>
     )
 }
