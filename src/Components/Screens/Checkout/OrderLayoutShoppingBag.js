622828ff2e6f752bf67188305bfd0e90c2e00b24
aniket salunke <aniket3984@gmail.com>
2024-01-23 23:26:29 +0530

    refactored ui

--- src/Components/Screens/Checkout/OrderLayoutShoppingBag.js
+++ src/Components/Screens/Checkout/OrderLayoutShoppingBag.js
@@ -36,102 +36,96 @@ export const OrderLayoutShoppingBag = () => {
     ];
 
     return (
-        <div className="shopping-bag-section">
-            <div className="shopping-bag p-4">
-                <div className="shopping-bag-header d-flex justify-content-between">
-                    <h4>YOUR SHOPPING BAG</h4>
-                    <Link to="#">EDIT BAG</Link>
-                </div>
-
-                <Divider style={{ backgroundColor: "#666666" }} />
-
-                <div className="cart-items-wrapper">
-                    {cartItems.map((product) => {
-                        return (
-                            <div key={product.id}>
-                                <Card className="cart-item p-3" sx={{ border: "none" }}>
-                                    <Grid container spacing={1}>
-                                        <Grid item xs={4}>
-                                            <div className="product-image">
-                                                <CardMedia
-                                                    component="img"
-                                                    alt={name}
-                                                    height="81"
-                                                    width="81"
-                                                    image={product.image}
-                                                />
-                                            </div>
+        <div className="shopping-bag-wrapper-section">
+            <div className="shopping-bag-section">
+                <div className="shopping-bag p-4">
+                    <div className="shopping-bag-header d-flex justify-content-between">
+                        <h4>YOUR SHOPPING BAG</h4>
+                        <Link to="#">EDIT BAG</Link>
+                    </div>
+                    <Divider style={{ backgroundColor: "#666666" }} />
+                    <div className="cart-items-wrapper">
+                        {cartItems.map((product) => {
+                            return (
+                                <div key={product.id}>
+                                    <Card className="cart-item p-3" sx={{ border: "none" }}>
+                                        <Grid container spacing={1}>
+                                            <Grid item xs={4}>
+                                                <div className="product-image">
+                                                    <CardMedia
+                                                        component="img"
+                                                        alt={name}
+                                                        height="81"
+                                                        width="81"
+                                                        image={product.image}
+                                                    />
+                                                </div>
+                                            </Grid>
+                                            <Grid item xs={4}>
+                                                <div className="product-information">
+                                                    <Typography variant="h6" component="div">
+                                                        {product.name}
+                                                    </Typography>
+                                                    <Typography
+                                                        variant="body2"
+                                                        fontWeight="bold"
+                                                        color="text.secondary"
+                                                    >
+                                                        &#8377; {product.type}
+                                                    </Typography>
+                                                    <Typography
+                                                        variant="body2"
+                                                        color="text.secondary"
+                                                    >
+                                                        Quantity: {product.quantity}
+                                                    </Typography>
+                                                </div>
+                                            </Grid>
                                         </Grid>
-
-                                        <Grid item xs={4}>
-                                            <div className="product-information">
-                                                <Typography variant="h6" component="div">
-                                                    {product.name}
-                                                </Typography>
-                                                <Typography
-                                                    variant="body2"
-                                                    fontWeight="bold"
-                                                    color="text.secondary"
-                                                >
-                                                    &#8377; {product.type}
-                                                </Typography>
-                                                <Typography
-                                                    variant="body2"
-                                                    color="text.secondary"
-                                                >
-                                                    Quantity: {product.quantity}
-                                                </Typography>
-                                            </div>
-                                        </Grid>
-                                    </Grid>
-                                </Card>
-                                <Divider style={{ backgroundColor: "#666666" }} />
+                                    </Card>
+                                    <Divider style={{ backgroundColor: "#666666" }} />
+                                </div>
+                            );
+                        })}
+                        <div className="w-100 mt-4 order-continue-to-checkout">
+                            <div className="apply-code-wrapper">
+                                <input
+                                    type="text"
+                                    className="w-100 apply-code-input"
+                                    placeholder="Enter coupon code"
+                                />
+                                <Button
+                                    variant="outlined"
+                                    className="outlined-black btn-apply-code"
+                                >
+                                    APPLY CODE &nbsp;
+                                    <EastIcon />
+                                </Button>
                             </div>
-                        );
-                    })}
-                    <div className="w-100 mt-4 order-continue-to-checkout">
-                        <div className="apply-code-wrapper">
-                            <input
-                                type="text"
-                                className="w-100 apply-code-input"
-                                placeholder="Enter coupon code"
-                            />
-
+                            <div className="totals">
+                                <div className="subtotal total-info">
+                                    <span>subtotal</span>
+                                    <span className="text-bold">&#8377; 190</span>
+                                </div>
+                                <div className="shipping-charges total-info">
+                                    <span>Shipping charges</span>
+                                    <span>calculated at next step.</span>
+                                </div>
+                                <div className="total total-info">
+                                    <span className="text-bold">Total</span>
+                                    <span className="text-bold">&#8377; 190</span>
+                                </div>
+                            </div>
                             <Button
-                                variant="outlined"
-                                className="outlined-black btn-apply-code"
+                                className="btn btn-block bg-black btn-submit btn-checkout"
+                                fullWidth
+                                variant="contained"
+                                type="submit"
                             >
-                                APPLY CODE &nbsp;
+                                CONTINUE TO CHECKOUT &nbsp;
                                 <EastIcon />
                             </Button>
                         </div>
-
-                        <div className="totals">
-                            <div className="subtotal total-info">
-                                <span>subtotal</span>
-                                <span className="text-bold">&#8377; 190</span>
-                            </div>
-
-                            <div className="shipping-charges total-info">
-                                <span>Shipping charges</span>
-                                <span>calculated at next step.</span>
-                            </div>
-
-                            <div className="total total-info">
-                                <span className="text-bold">Total</span>
-                                <span className="text-bold">&#8377; 190</span>
-                            </div>
-                        </div>
-
-                        <Button
-                            className="btn btn-block bg-black btn-submit btn-checkout"
-                            fullWidth
-                            variant="contained"
-                            type="submit"
-                        >
-                            CONTINUE TO CHECKOUT &nbsp;
-                            <EastIcon />
-                        </Button>
                     </div>
                 </div>
             </div>
--- src/ExpansionCartView.js
+++ src/ExpansionCartView.js
@@ -8,35 +8,35 @@ import { Badge, Typography } from "@mui/material";
 import { OrderLayoutShoppingBag } from "./Components/Screens/Checkout/OrderLayoutShoppingBag";
 
 export const ExpansionCartView = () => {
-  const styles = {
-    accordion: {
-      backgroundColor: '#F0EEE5',
-    },
-  };
-  
-  return (
-    <div className="expansion-cart-view-wrapper">
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ArrowDownwardIcon />}
-          style={styles.accordion}
-          aria-controls="panel1-content"
-          id="panel1-header"
-        >
-          <Typography>
-            <Badge badgeContent={4} color="primary">
-              <img
-                src={shoppingBagLogo}
-                style={{ height: "20px", width: "20px" }}
-              />
-            </Badge>
-            Order summary
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-         <OrderLayoutShoppingBag />
-        </AccordionDetails>
-      </Accordion>
-    </div>
-  );
+    const styles = {
+        accordion: {
+            backgroundColor: '#F0EEE5',
+        },
+    };
+
+    return (
+        <div className="expansion-cart-view-wrapper">
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ArrowDownwardIcon />}
+                    style={styles.accordion}
+                    aria-controls="panel1-content"
+                    id="panel1-header"
+                >
+                    <Typography>
+                        <Badge badgeContent={4} color="primary">
+                            <img
+                                src={shoppingBagLogo}
+                                style={{ height: "20px", width: "20px" }}
+                            />
+                        </Badge>
+                        Order summary
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <OrderLayoutShoppingBag />
+                </AccordionDetails>
+            </Accordion>
+        </div>
+    );
 };
