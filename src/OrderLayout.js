cfd6c6738a1e3985b6dbe40ce8e055a24f0ce928
aniket salunke <aniket3984@gmail.com>
2024-02-01 22:29:48 +0530

    formatting

--- src/OrderLayout.js
+++ src/OrderLayout.js
@@ -20,11 +20,11 @@ import { OrderLayoutShoppingBag } from "./Components/Screens/Checkout/OrderLayou
 const OrderLayout = () => {
   return (
     // <div className="order-layout d-flex justify-content-center">
-      <Grid container  className="order-main-wrapper" spacing={3}>
+      <Grid container className="order-main-wrapper" spacing={3}>
         <Grid item xs={12} md={6} className="form-section p-5">
           <div className="form-section-wrapper">
             <div className="checkout-header">
-              <img src={siteLogo} alt="" />
+              <img src={siteLogo} alt=""/>
             </div>
 
             <ExpansionCartView style={{display: 'none'}} className="expansion-cart-view" />
