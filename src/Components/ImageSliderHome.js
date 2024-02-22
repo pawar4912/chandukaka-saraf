3668130c2a31d052bcfa323ec39263ac33245faf
aniket salunke <aniket3984@gmail.com>
2024-02-23 00:14:39 +0530

    refactored buttons

--- src/Components/ImageSliderHome.js
+++ src/Components/ImageSliderHome.js
@@ -1,49 +1,47 @@
 import * as React from 'react';
 import { useSnapCarousel } from 'react-snap-carousel';
-import PropTypes, { element } from 'prop-types';
 import ImageSliderCardHome from './ImageSliderCardHome';
-import LeftIcon from '../images/icons/sliderLeftIcon.svg';
-import RightIcon from '../images/icons/sliderRightIcon.svg';
 import useWindowWidthAndHeight from '../utilities/CustomHooks';
-import { Grid } from '@mui/material';
+import EastIcon from "@mui/icons-material/East";
+import WestIcon from "@mui/icons-material/West";
 import _ from 'lodash';
 
-function ImageSliderHome({ height, width, CardImage,padding, CardImageType, className, imagesArray }) {
+function ImageSliderHome({ height, width, CardImage, padding, CardImageType, className, imagesArray }) {
   const {
-    scrollRef, pages,activePageIndex, next, prev, goTo
+    scrollRef, pages, activePageIndex, next, prev, goTo
   } = useSnapCarousel();
   const windoDimensions = useWindowWidthAndHeight();
   return (
     <>
-      { windoDimensions[0]  >= 768 && <div className={(padding ==0 ? ('image-slider-navigation-icon image-slider-navigation-icon-no-width'): 'image-slider-navigation-icon' )}>
-        <button className="arrow-icon-generic" type="button" onClick={() => prev()}>
-          <img src={LeftIcon} alt="React Logo" />
+      {windoDimensions[0] >= 768 && <div className={(padding == 0 ? ('image-slider-navigation-icon image-slider-navigation-icon-no-width') : 'image-slider-navigation-icon')}>
+        <button className="arrow-icon-generic round-border" type="button" onClick={() => prev()}>
+          <WestIcon/>
         </button>
-        <button className="arrow-icon-generic" type="button" onClick={() => next()}>
-          <img src={RightIcon} alt="React Logo" />
+        <button className="arrow-icon-generic round-border" type="button" onClick={() => next()}>
+          <EastIcon />
         </button>
       </div>}
       <ul
         className={`image-slider-component ${className}`}
         ref={scrollRef}
       >
-        { CardImageType== "Favourites" && windoDimensions[0] <=768 ?_.chunk(imagesArray, 4).map((item, key)=> (
-              <div className="item-section" key={key}>
-                {item.map((inneritem, index) => (
-                <ImageSliderCardHome CardImageType={CardImageType} key={index} CardImage={inneritem} />
-              ))}
-              </div>
+        {CardImageType == "Favourites" && windoDimensions[0] <= 768 ? _.chunk(imagesArray, 4).map((item, key) => (
+          <div className="item-section" key={key}>
+            {item.map((inneritem, index) => (
+              <ImageSliderCardHome CardImageType={CardImageType} key={index} CardImage={inneritem} />
+            ))}
+          </div>
         )) : imagesArray.map((item, index) => (
-              <ImageSliderCardHome CardImageType={CardImageType} height={height} width={width} key={index} CardImage={item} padding={padding}/>
-          ))}
+          <ImageSliderCardHome CardImageType={CardImageType} height={height} width={width} key={index} CardImage={item} padding={padding} />
+        ))}
       </ul>
-      { CardImageType== "Favourites" && windoDimensions[0] <768  && <ol style={{ display: 'flex', justifyContent: "center", paddingLeft: "0", marginTop: 24, marginBottom: 0 }}>
+      {CardImageType == "Favourites" && windoDimensions[0] < 768 && <ol style={{ display: 'flex', justifyContent: "center", paddingLeft: "0", marginTop: 24, marginBottom: 0 }}>
         {pages.map((_, i) => (
-            <button key={i} className='image-scroll-navigation'
-              style={i === activePageIndex ? { opacity: 0.5 } : {}}
-              onClick={() => goTo(i)}
-            >
-            </button>
+          <button key={i} className='image-scroll-navigation'
+            style={i === activePageIndex ? { opacity: 0.5 } : {}}
+            onClick={() => goTo(i)}
+          >
+          </button>
         ))}
       </ol>}
     </>
