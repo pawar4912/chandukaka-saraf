e9c0f17090ef32b79a87bdc7338f24b23c1f6bf9
pawar4912 <86315952+pawar4912@users.noreply.github.com>
2024-02-25 14:50:25 +0530

    #79 banner images for mobile

--- src/Components/CarouselHome.js
+++ src/Components/CarouselHome.js
@@ -7,22 +7,28 @@ import useWindowWidthAndHeight from '../utilities/CustomHooks';
 import img1 from '../images/banners/desktop/1.jpg';
 import img2 from '../images/banners/desktop/2.jpg';
 import img3 from '../images/banners/desktop/3.jpg';
+import imgMob1 from '../images/banners/mobile/1.jpg';
+import imgMob2 from '../images/banners/mobile/2.jpg';
+import imgMob3 from '../images/banners/mobile/3.jpg';
 
 const items = [
   {
     name: 'Elegance Redefined:',
     description: 'Discover exquisite jewellery that reflects your inner radiance',
     imagepath: img1,
+    mobileImagePath: imgMob1,
   },
   {
     name: 'Elegance Redefined:',
     description: 'Discover exquisite jewellery that reflects your inner radiance',
     imagepath: img2,
+    mobileImagePath: imgMob2,
   },
   {
     name: 'Elegance Redefined:',
     description: 'Discover exquisite jewellery that reflects your inner radiance',
     imagepath: img3,
+    mobileImagePath: imgMob3,
   },
 ];
 
@@ -68,7 +74,7 @@ function CarouselHome() {
           {
             items.map((item) => (
               <Paper key={item.name} className="carouselPaper" >
-                <img className="carousel-img" src={windoDimensions[0] <= 768 ? `${imageMobileView}`:  `${item.imagepath}` } />
+                <img className="carousel-img" src={windoDimensions[0] <= 768 ? `${item.mobileImagePath}`:  `${item.imagepath}` } />
                 <div className="service-slide-text-wrapper">
                   <h2 className="service-slide-text">{item.name}</h2>
                   <p className="service-slide-description">{item.description}</p>
