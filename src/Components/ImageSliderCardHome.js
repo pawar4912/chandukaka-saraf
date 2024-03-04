d91239a4158f3b81cbf0f9d298b060e2c765d4ed
pawar4912 <86315952+pawar4912@users.noreply.github.com>
2024-03-04 10:43:45 +0530

    fixed redirection

--- src/Components/ImageSliderCardHome.js
+++ src/Components/ImageSliderCardHome.js
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 export default function ImageSliderCardHome({width, height, CardImage, padding, CardImageType, name}) {
   const navigation = useNavigate()
   return (
-    <div onClick={()=>navigation(`bullions/${name}`)} className={ "" +CardImageType + "-item-section image-slide-card-container"}>
+    <div className={ "" +CardImageType + "-item-section image-slide-card-container"}>
       <img width={width} height={height}  src={`${CardImage}`} />
       { padding ==0 && <Divider variant="middle" />}
       <p className={ padding ==0  ? "image-slider-card-text no-border": "image-slider-card-text"}>
